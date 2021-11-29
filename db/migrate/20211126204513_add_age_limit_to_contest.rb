fa2ec0839185a3537a2b2de7bb91f669113fdf64
Bryant Morrill <bryantmorrill@bryants-mbp.lan>
2021-11-29 14:00:57 -0700

    allow null on age_limit

--- db/migrate/20211126204513_add_age_limit_to_contest.rb
+++ db/migrate/20211126204513_add_age_limit_to_contest.rb
@@ -1,5 +1,5 @@
 class AddAgeLimitToContest < ActiveRecord::Migration[7.0]
   def change
-    add_column :contests, :age_limit, :integer
+    add_column :contests, :age_limit, :integer, null: true
   end
 end
